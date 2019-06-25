81b5e42e3c4c19935ff6ec4d06baae76faa8c3b6
Yainelys Rivero <yainelyz@gmail.com>
2019-06-25 16:34:46 -0400

    Configure Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC-12E1S4-a68Oz46wzqV4ahe7-MPuy9b4',
+    authDomain: 'natureshopper.firebaseapp.com',
+    databaseURL: 'https://natureshopper.firebaseio.com',
+    projectId: 'natureshopper',
+    storageBucket: '',
+    messagingSenderId: '400355880168',
+    appId: '1:400355880168:web:cf8459a2df14fadc'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC-12E1S4-a68Oz46wzqV4ahe7-MPuy9b4',
+    authDomain: 'natureshopper.firebaseapp.com',
+    databaseURL: 'https://natureshopper.firebaseio.com',
+    projectId: 'natureshopper',
+    storageBucket: '',
+    messagingSenderId: '400355880168',
+    appId: '1:400355880168:web:cf8459a2df14fadc'
+  }
 };
 
 /*
