db3c7332ee346d0f7193d83bf661c6b5ec14f47a
Yainelys Rivero <yainelyz@gmail.com>
2019-07-09 15:15:48 -0400

    Extracting Product interface

--- src/app/admin/components/admin-products/admin-products.component.ts
+++ src/app/admin/components/admin-products/admin-products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ProductService } from './../../../shared/services/product.service';
+import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,12 +9,16 @@ import { ProductService } from './../../../shared/services/product.service';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: any[] = [];
-  filteredProducts: any[] = [];
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.subscription = productService.getAll().subscribe((products: any) => this.filteredProducts = this.products = products);
+    this.subscription = productService.getAll().subscribe((p) => {
+    this.products = this.filteredProducts = p.map(o => {
+        return { id: o.key, ...o.payload.val() };
+      });
+    });
   }
 
   ngOnInit() {
@@ -26,7 +31,7 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   filter(query: string) {
     console.log(this.products);
     this.filteredProducts = (query) ?
-      this.products.filter(p => p.payload.val().title.toLowerCase().includes(query.toLowerCase()))
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
       : this.products;
   }
 
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,5 +1,7 @@
+import { Product } from './../models/product';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class ProductService {
   }
 
   getAll() {
-    return this.db.list('/products').snapshotChanges();
+    return this.db.list<Product>('/products').snapshotChanges();
   }
 
   get(id) {
