65b96cc0e60a06b1573305917f7eff12d564657b
Yainelys Rivero <yainelyz@gmail.com>
2019-07-15 16:54:20 -0400

    Filtering Products by Category

--- src/app/shopping/components/products/products.component.ts
+++ src/app/shopping/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { CategoryService } from 'src/app/shared/services/category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/shared/models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,12 +10,27 @@ import { CategoryService } from 'src/app/shared/services/category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) {
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService,
+    categoryService: CategoryService) {
+    productService.getAll().subscribe(products => {
+      this.products = this.filteredProducts = products.map(p => {
+        return { id: p.key, ...p.payload.val() };
+      });
+    });
+
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ? this.products.filter(p => p.category === this.category) : this.products;
+    });
   }
 
   ngOnInit() {
